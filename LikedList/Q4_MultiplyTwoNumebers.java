4cabcd5246fb74da782e1990fa59a855dc90cf03
Sanjeevi <sanjeevisakthivel2004@gmail.com>
2023-07-16 22:48:25 +0530

    Day 14/45

---
+++ LikedList/Q4_MultiplyTwoNumebers.java
@@ -0,0 +1,33 @@
+package LikedList;
+
+public class Q4_MultiplyTwoNumebers {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    static long multiplyTwoLists(Node first, Node second) {
+        long N = 1000000007;
+        long num1 = 0, num2 = 0;
+
+        while (first != null || second != null) {
+
+            if (first != null) {
+                num1 = ((num1) * 10) % N + first.data;
+                first = first.next;
+            }
+
+            if (second != null) {
+                num2 = ((num2) * 10) % N + second.data;
+                second = second.next;
+            }
+
+        }
+        return ((num1 % N) * (num2 % N)) % N;
+    }
+}
