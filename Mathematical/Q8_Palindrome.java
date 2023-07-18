85b2f3d6015f0e948e250af7569fdf3f2e5db621
Sanjeevi <sanjeevisakthivel2004@gmail.com>
2023-07-18 22:27:57 +0530

    Day 16/45

---
+++ Mathematical/Q8_Palindrome.java
@@ -0,0 +1,16 @@
+package Mathematical;
+
+public class Q8_Palindrome {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
