13d9a4e743e2093f0e9ba4693dcef92e161e8579
Sanjeevi <sanjeevisakthivel2004@gmail.com>
2023-08-03 00:02:52 +0530

    Day 31/45

---
+++ Backtracking/Q2_PartionProblem.java
@@ -0,0 +1,36 @@
+package Backtracking;
+
+public class Q2_PartionProblem {
+    static boolean isSubsetSum(int arr[], int n, int sum)
+    {
+       
+        if (sum == 0)
+            return true;
+        if (n == 0 && sum != 0)
+            return false;
+ 
+       
+        if (arr[n - 1] > sum)
+            return isSubsetSum(arr, n - 1, sum);
+ 
+        
+        return isSubsetSum(arr, n - 1, sum)
+            || isSubsetSum(arr, n - 1, sum - arr[n - 1]);
+    }
+ 
+  
+    static boolean findPartition(int arr[], int n)
+    {
+       
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += arr[i];
+ 
+        
+        if (sum % 2 != 0)
+            return false;
+ 
+       
+        return isSubsetSum(arr, n, sum / 2);
+    }
+}
